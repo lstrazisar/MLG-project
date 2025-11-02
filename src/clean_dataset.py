7844c2dac6ada717856742b15deb8ede5a33de43
lstrazisar <luka.strazisar2@gmail.com>
2025-11-02 11:29:06 +0100

    primer pretvorbe dataseta v grafe

--- src/clean_dataset.py
+++ src/clean_dataset.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 dataset = pd.read_csv('./data/DB for chromophore_Sci_Data_rev02.csv')
 clean_dataset = dataset.iloc[:, [0, 1, 2, 3, 4]].dropna()
+# drop rows with Solvent="gas"
+clean_dataset = clean_dataset[clean_dataset['Solvent'] != 'gas']
+
 clean_dataset.to_csv('./data/clean_chromophore_data.csv', index=False)
